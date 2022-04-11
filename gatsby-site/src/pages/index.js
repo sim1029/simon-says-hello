4b091337a46114a978d313213c31de0f741c59bd
sim1029 <sps67@pitt.edu>
2022-04-11 12:16:10 -0400

    initialize gatsby project

---
+++ gatsby-site/src/pages/index.js
@@ -0,0 +1,23 @@
+import React from "react"
+import { Helmet } from "react-helmet"
+import { Link } from "gatsby"
+
+import "../styles/bootstrap.min.css"
+import "../styles/bootstrap.min.css"
+import "../styles/mainStyles.css"
+
+export default function Home() {
+  return (
+    <>
+      <Helmet>
+        <meta charset="UTF-8"/>
+        <meta http-equiv="X-UA-Compatible" content="IE=edge"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+        <title>Simon Says Hello</title>
+      </Helmet>
+      <div>
+        <h1>INSERT PAGE HERE LOL</h1>
+      </div>
+    </>
+  )
+}
