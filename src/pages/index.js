b8dc79d5e51b7d570aea8db4dfec0216c13a471a
sim1029 <sps67@pitt.edu>
2022-05-11 10:46:52 -0700

    Gut the template like a pig and add bootstrap instead of smelly bulma

---
+++ src/pages/index.js
@@ -0,0 +1,20 @@
+import React from "react";
+import Layout from "../components/Layout";
+
+// eslint-disable-next-line
+const Index = () => {
+  return (
+    <Layout>
+      <section>
+        <div>
+          <h1>Splash Page!</h1>
+          <button type="button" className="btn btn-primary">
+            Primary
+          </button>
+        </div>
+      </section>
+    </Layout>
+  );
+};
+
+export default Index
