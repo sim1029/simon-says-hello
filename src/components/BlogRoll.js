da278c7799282e75b67a74336eb6472a4928826f
sim1029 <sps67@pitt.edu>
2022-05-24 14:12:22 -0400

    Get up to date since switching dev environments

--- src/components/BlogRoll.js
+++ src/components/BlogRoll.js
@@ -1,6 +1,7 @@
 import { graphql, useStaticQuery } from 'gatsby'
 import React from 'react'
 import PreviewCompatibleImage from './PreviewCompatibleImage'
+import { Link } from 'gatsby'
 
 const BlogRoll = () => {
   const data = useStaticQuery(graphql`
@@ -39,25 +40,27 @@ const BlogRoll = () => {
 
   const SpotlightBlog = ({ edge }) => {
     return (
-      <div className='flex ml-10 flex-col items-center'>
+      <div className='ml-10 flex flex-col items-center'>
         <h1 className='mt-10 self-start text-3xl font-bold'>Spotlight Post</h1>
-        <div className='group mt-10 flex w-3/4 items-center justify-center space-x-10 rounded-xl shadow-md bg-slate-300 p-12 hover:cursor-pointer dark:bg-gray-700'>
-          <div className='flex w-[400px] skew-y-3 flex-col items-center bg-blue  dark:bg-red'>
-            <div
-              className='prose prose-sm prose-slate w-[400px] translate-x-1 -translate-y-1 bg-white p-4 transition-transform duration-200 ease-out group-hover:translate-x-3 group-hover:-translate-y-3'
-              dangerouslySetInnerHTML={{ __html: edge.node.excerpt }}
-            />
+        <Link to={edge.node.fields.slug} className='contents'>
+          <div className='group mt-10 flex w-3/4 items-center justify-center space-x-10 rounded-xl bg-slate-300 p-12 shadow-md hover:cursor-pointer dark:bg-gray-700'>
+            <div className='flex w-[400px] skew-y-3 flex-col items-center bg-blue  dark:bg-red'>
+              <div
+                className='prose prose-sm prose-slate w-[400px] translate-x-1 -translate-y-1 bg-white p-4 transition-transform duration-200 ease-out group-hover:translate-x-3 group-hover:-translate-y-3'
+                dangerouslySetInnerHTML={{ __html: edge.node.excerpt }}
+              />
+            </div>
+            <div className='flex flex-col'>
+              <h1 className='text-2xl font-bold'>
+                {edge.node.frontmatter.title}
+              </h1>
+              <p className='mt-8'>{edge.node.frontmatter.description}</p>
+              <p className='mx-2 my-8 self-end underline decoration-blue decoration-2 underline-offset-4 opacity-0 transition duration-500 ease-linear group-hover:opacity-100 dark:decoration-red'>
+                Keep Reading <span>&rarr;</span>
+              </p>
+            </div>
           </div>
-          <div className='flex flex-col'>
-            <h1 className='text-2xl font-bold'>
-              {edge.node.frontmatter.title}
-            </h1>
-            <p className='mt-8'>{edge.node.frontmatter.description}</p>
-            <p className='mx-2 my-8 self-end underline decoration-blue decoration-2 underline-offset-4 opacity-0 transition duration-500 ease-linear group-hover:opacity-100 dark:decoration-red'>
-              Keep Reading <span>&rarr;</span>
-            </p>
-          </div>
-        </div>
+        </Link>
       </div>
     )
   }
@@ -71,7 +74,7 @@ const BlogRoll = () => {
             if (index != 0) {
               return (
                 <div
-                  className='flex flex-col items-center rounded-lg bg-slate-300 p-8 shadow-md dark:bg-gray-700'
+                  className='flex flex-col items-center rounded-lg bg-slate-300 p-4 shadow-md dark:bg-gray-700'
                   key={post.id}
                 >
                   <article>
