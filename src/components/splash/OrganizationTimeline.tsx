33f53cd132b33338bf99a8e6e498c74a76d64f89
sim1029 <sps67@pitt.edu>
2022-07-30 19:29:10 -0400

    stub organizations section

---
+++ src/components/splash/OrganizationTimeline.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const OrganizationTimeline = () => {
+  return (
+    <div>OrganizationTimeline</div>
+  )
+}
+
+export default OrganizationTimeline
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Layout from "../components/layout/Layout";
+import OrganizationTimeline from "../components/splash/OrganizationTimeline";
 import SplashHeader from "../components/splash/SplashHeader";
 
 const Index = () => {
 	return (
 		<Layout>
-			<div className="my-8 flex flex-col">
+			<div className="my-8 flex flex-col items-center space-y-8">
 				<SplashHeader />
+				<OrganizationTimeline />
 			</div>
 		</Layout>
 	);
