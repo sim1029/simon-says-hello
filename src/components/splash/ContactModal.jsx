330b4b08dc8e9c910605993af50550d23cfc5191
sim1029 <sps67@pitt.edu>
2023-07-14 17:10:11 -0400

    fix api gateway

--- src/components/splash/ContactModal.jsx
+++ src/components/splash/ContactModal.jsx
@@ -28,7 +28,6 @@ const ContactModal = () => {
 
 		fetch("https://3j2yibwlqk.execute-api.us-east-1.amazonaws.com/prod-ssh/", {
 			method: "POST",
-			mode: "no-cors",
 			headers: {
 				"Content-Type": "application/json",
 				"x-api-key": process.env.GATSBY_CONTACT_API_KEY,
