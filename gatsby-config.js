5a092e1d3f8040a89344a07170df1b6649731d6c
sim1029 <sps67@pitt.edu>
2023-07-20 23:57:26 -0400

    include all fields in remark

--- gatsby-config.js
+++ gatsby-config.js
@@ -64,12 +64,6 @@ module.exports = {
 							// [Optional] The root of "media_folder" in your config.yml
 							// Defaults to "static"
 							staticFolderName: "static",
-							// [Optional] Include the following fields, use dot notation for nested fields
-							// All fields are included by default
-							include: ["featured"],
-							// [Optional] Exclude the following fields, use dot notation for nested fields
-							// No fields are excluded by default
-							exclude: ["featured.skip"],
 						},
 					},
 					{
